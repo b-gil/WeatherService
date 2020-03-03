5c3b7abf8d10c2c4c84c33138eb48041e89f97cc
Demigod46 <61291591+Demigod46@users.noreply.github.com>
2020-03-03 23:06:25 +0600

    aye

---
+++ src/test/java/com/example/weatherapplication/demo/WeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherapplication.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
